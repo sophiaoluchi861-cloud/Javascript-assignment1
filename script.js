f8313730ce380d7774f61641c9a796327bf51916
nnanna oluchi <sophiaoluchi861@gmail.com>
2026-06-20 23:42:08 -0700

    javascript assignment done

---
+++ script.js
@@ -0,0 +1,45 @@
+
+let yourName = "Oluchi";  
+let yourAge = 18;         
+let yourSchool = "Genesys"; 
+//wrote genesy because i have not yet started schooling
+console.log(yourName);
+console.log(yourAge);
+console.log(yourSchool);
+
+let studentName = "sophia"; 
+let score = 85;              
+let isPresent = true;        
+
+console.log(studentName);
+console.log(score);
+console.log(isPresent);
+
+let price = 5000;
+let quantity = 5;
+
+console.log( price + quantity);        
+console.log( price - quantity);     
+console.log( price * quantity);  
+console.log( price / quantity);      
+
+let firstName = "David";
+let lastName = "Peter";
+let age = 20;
+
+console.log(`My name is ${firstName} ${lastName} and I am ${age} years old`);
+let mark = 75; // change this to test
+
+if (score >= 70) {
+  console.log("Excellent");
+} else if (score >=50) {
+  console.log("Pass");
+} else {
+  console.log("Fail");
+}
+ for (let i = 1; i<=20; i++){
+console.log(i)
+ }
+ 
+  
+
